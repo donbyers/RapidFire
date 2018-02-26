d766c92c532cf28068720b3ffd5c20e753a6e5fa
Trent Lugar -X (tlugar - RANDSTAD NORTH AMERICA INC at Cisco) <tlugar@cisco.com>
2018-02-26 17:41:42 -0500

    removed get devicegroup by name

--- new_api_script.py
+++ new_api_script.py
@@ -109,6 +109,7 @@ def fmc_api_get(api_url):
 		else:
 			r.raise_for_status()
 			print("Error occurred in GET --> "+resp)
+			json_resp = None
 	except requests.exceptions.HTTPError as err:
 		print ("Error in connection --> "+str(err)) 
 	finally:
@@ -127,28 +128,28 @@ policy_obj=fmc_api_get(url)
 policy_id=policy_obj['items'][0]['id']
 
 #SEARCH FOR PROVIDED DEVICE GROUP 
-api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devicegroups/devicegrouprecords?name=" + fmc_devicegroup_name
+#api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devicegroups/devicegrouprecords?name=" + fmc_devicegroup_name
+#url = server + api_path
+#if (url[-1] == '/'):
+#	url = url[:-1]
+#print("Searching for device group " + fmc_devicegroup_name + "...")
+#group_search=fmc_api_get(url)
+#if(group_search['items']):
+#	print("FOUND device group with name of " + fmc_devicegroup_name)
+#	device_group_id = group_search['items'][0]['id']
+#else:
+#print("Provided device group name NOT FOUND, creating device group " + fmc_devicegroup_name)
+# POST/CREATE DEVICE GROUP OPERATION
+api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devicegroups/devicegrouprecords"
 url = server + api_path
 if (url[-1] == '/'):
 	url = url[:-1]
-print("Searching for device group " + fmc_devicegroup_name + "...")
-group_search=fmc_api_get(url)
-if(group_search['items']):
-	print("FOUND device group with name of " + fmc_devicegroup_name)
-	device_group_id = group_search['items'][0]['id']
-else:
-	print("Provided device group name NOT FOUND, creating device group " + fmc_devicegroup_name)
-	# POST/CREATE DEVICE GROUP OPERATION
-	api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devicegroups/devicegrouprecords"
-	url = server + api_path
-	if (url[-1] == '/'):
-		url = url[:-1]
-	post_data = {
-		"name": fmc_devicegroup_name,
-	}
-	print("POSTing device group " + fmc_devicegroup_name+"...")
-	group_obj = fmc_api_post(url,post_data)
-	device_group_id = group_obj['id']
+post_data = {
+	"name": fmc_devicegroup_name,
+}
+print("POSTing device group " + fmc_devicegroup_name+"...")
+group_obj = fmc_api_post(url,post_data)
+device_group_id = group_obj['id']
 
 # POST/CREATE DEVICE OPERATION
 api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devices/devicerecords"
